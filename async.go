9e05d9d7abe434fd0b87bcbbc9f49b547c8bbb86
codemodify <codemodify@member.fsf.org>
2020-04-04 03:18:48 -0700

    adding async

---
+++ async.go
@@ -0,0 +1,24 @@
+package mixers
+
+import (
+	logging "github.com/codemodify/systemkit-logging"
+)
+
+type asyncLogger struct {
+	logger logging.Logger
+}
+
+// NewAsync -
+func NewAsync(logger logging.Logger) logging.Logger {
+	return &asyncLogger{
+		logger: logger,
+	}
+}
+
+func (thisRef asyncLogger) Log(logEntry logging.LogEntry) logging.LogEntry {
+	go func(theLogEntryCopy logging.LogEntry) {
+		thisRef.logger.Log(theLogEntryCopy)
+	}(logEntry)
+
+	return logging.LogEntry{}
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+`systemkit-logging-mixers-async` is a log formatting component from `github.com/codemodify/systemkit-logging`
